00ee3dd9d029951f85cc034e1fbfcff7dde15f59
Somnath Nandi <123479436+CodeOfSomnath@users.noreply.github.com>
2023-02-08 11:48:18 +0530

    Adding insertion method

--- LinkedList.h
+++ LinkedList.h
@@ -1,57 +1,58 @@
 #ifndef LINKED_LIST_H
-
 #define LINKED_LIST_H
 
 #include <stdio.h>
-
 #include <stdlib.h>
 
 struct Node {
-
   int data;
-
   struct Node *next;
-
 };
-
 typedef struct Node Node;
 
 Node *init(Node *ptr, int data) {
-
   ptr->data = data;
-
   ptr->next = (Node *)malloc(sizeof(Node));
-
   return ptr->next;
-
 }
 
 // it will update your node
-
 Node *add(Node *ptr, int *size, int data) {
-
   ptr->data = data;
-
   ptr->next = (Node *)malloc(sizeof(Node));
 
   *size += 1;
-
   return ptr->next;
-
 }
 
 void print(Node *head, int *size) {
-
   Node *temp = head->next;
-
+  printf("[");
   for (int i = 0; i < *size; i++) {
-
-    printf("%d\n", temp->data);
-
+    if (i == *size - 1) {
+      printf("%d", temp->data);
+    } else {
+      printf("%d, ", temp->data);
+    }
     temp = temp->next;
-
   }
+  printf("]\n");
+}
 
+void insert(Node *head, int *size, int index, int data) {
+  Node *fast;
+  Node *node = (Node *)malloc(sizeof(Node));
+  Node *last;
+  Node *temp = head->next;
+  for (int i = 0; i < index - 1; i++) {
+    temp = temp->next;
+  }
+  fast = temp;
+  last = fast->next;
+  fast->next = node;
+  node->next = last;
+  node->data = data;
+  *size += 1;
 }
 
 #endif
